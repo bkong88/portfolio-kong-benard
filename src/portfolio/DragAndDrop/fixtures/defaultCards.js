468c81ad700ee89f89f9703c1576133a5cee83f1
Benard Kong <benardkong@hotmail.com>
2020-01-11 11:09:41 -0800

    [dnd-1-init] Create Instructions

--- src/portfolio/DragAndDrop/fixtures/defaultCards.js
+++ src/portfolio/DragAndDrop/fixtures/defaultCards.js
@@ -16,11 +16,77 @@ export const createDefaultCards = (listId, listTitle, numCards) => {
   const cardsObject = {}
 
   for (let i = 1; i <= numCards; i++) {
-    const newCard = createCard(listId, `card ${i} for ${listTitle}`)
+    const newCard = createCard(listId, `Example card ${i} for ${listTitle}`)
     cardsObject[newCard.cardId] = newCard.cardObject
 
     cardIdsArray.push(newCard.cardId)
   }
+
+  return { cardsObject, cardIdsArray }
+}
+
+const createAndInsertInObjectAndArray = (cardsObj, cardIdsArr, listId, content) => {
+  const newCard = createCard(listId, content)
+  cardsObj[newCard.cardId] = newCard.cardObject
+  cardIdsArr.push(newCard.cardId)
+}
+
+export const createInstructionCards = (listId) => {
+  const cardsObject = {}
+  const cardIdsArray = []
+
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'Welcome to my Drag and Drop App!')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'You can click and drag any card you want.')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'After dragging, drop it into another position.')
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'You can drop it into another list if you want too!'
+  )
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'You can also drag and drop lists by clicking and dragging the title of the lists.'
+  )
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'Edit card and list titles by clicking on them (without dragging).'
+  )
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'While editing, click on the trash icon to delete the card/list.'
+  )
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'Add new cards by clicking on the "Add new card" button at the bottom of each list'
+  )
+  createAndInsertInObjectAndArray(
+    cardsObject,
+    cardIdsArray,
+    listId,
+    'Add new lists by clicking on the "Add new List" button on the right'
+  )
+
+  return { cardsObject, cardIdsArray }
+}
+
+export const createTechStackCards = (listId) => {
+  const cardsObject = {}
+  const cardIdsArray = []
+
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'react-beautiful-dnd')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'react-icons')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'uuid')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'classnames')
+  createAndInsertInObjectAndArray(cardsObject, cardIdsArray, listId, 'lodash (for deep cloning)')
 
   return { cardsObject, cardIdsArray }
 }
--- src/portfolio/DragAndDrop/fixtures/defaultLists.js
+++ src/portfolio/DragAndDrop/fixtures/defaultLists.js
@@ -1,5 +1,5 @@
 import uuid_v1 from 'uuid/v1'
-import { createDefaultCards } from './defaultCards'
+import { createDefaultCards, createInstructionCards, createTechStackCards } from './defaultCards'
 
 const createList = (title = '', cardIdsArray = []) => {
   const listId = uuid_v1()
@@ -11,26 +11,45 @@ const createList = (title = '', cardIdsArray = []) => {
 
 export const createDefaultLists = () => {
   const listsObject = {}
+
+  // Create lists for instructions
+  const instructionsList = createList('Instructions')
+  const techStackList = createList('Packages Used (for developers)')
+  listsObject[instructionsList.listId] = instructionsList.listObject
+  listsObject[techStackList.listId] = techStackList.listObject
+
+  // Create lists
   const list1 = createList('Backlog')
   const list2 = createList('In Progress')
-  const list3 = createList('Complete')
+  const list3 = createList('Completed')
   listsObject[list1.listId] = list1.listObject
   listsObject[list2.listId] = list2.listObject
   listsObject[list3.listId] = list3.listObject
 
-  const listsArray = [list1.listId, list2.listId, list3.listId]
+  const listsArray = [instructionsList.listId, techStackList.listId, list1.listId, list2.listId, list3.listId]
+
+  // Add cards for instructions
+  const instructionsListCards = createInstructionCards(instructionsList.listId)
+  const techStackListCards = createTechStackCards(techStackList.listId)
+  listsObject[instructionsList.listId].cardIdsArray = instructionsListCards.cardIdsArray
+  listsObject[techStackList.listId].cardIdsArray = techStackListCards.cardIdsArray
 
   // Add cards
-  const list1Cards = createDefaultCards(list1.listId, list1.listObject.title, 8)
-  const list2Cards = createDefaultCards(list2.listId, list2.listObject.title, 15)
-  const list3Cards = createDefaultCards(list3.listId, list3.listObject.title, 7)
-
+  const list1Cards = createDefaultCards(list1.listId, list1.listObject.title, 4)
+  const list2Cards = createDefaultCards(list2.listId, list2.listObject.title, 12)
+  const list3Cards = createDefaultCards(list3.listId, list3.listObject.title, 6)
   listsObject[list1.listId].cardIdsArray = list1Cards.cardIdsArray
   listsObject[list2.listId].cardIdsArray = list2Cards.cardIdsArray
   listsObject[list3.listId].cardIdsArray = list3Cards.cardIdsArray
 
   // Combine all cards objects
-  const cardsObject = { ...list1Cards.cardsObject, ...list2Cards.cardsObject, ...list3Cards.cardsObject }
+  const cardsObject = {
+    ...instructionsListCards.cardsObject,
+    ...techStackListCards.cardsObject,
+    ...list1Cards.cardsObject,
+    ...list2Cards.cardsObject,
+    ...list3Cards.cardsObject,
+  }
 
   return { listsObject, listsArray, cardsObject }
 }
