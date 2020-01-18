4e001b1e82a021710786e70f7c152e919350e7f3
Benard Kong <benardkong@hotmail.com>
2020-01-18 13:06:23 -0800

    [dnd-3-link-with-splash-page] Added dnd illustration gif

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,7 @@ import PageFooter from '../components/PageFooter'
 import SideBar from '../components/SideBar'
 import config from '../../config'
 
-import pic2 from '../assets/images/pic02.jpg'
-import pic3 from '../assets/images/pic03.jpg'
-import pic4 from '../assets/images/pic04.jpg'
-import pic5 from '../assets/images/pic05.jpg'
-import pic6 from '../assets/images/pic06.jpg'
-import pic7 from '../assets/images/pic07.jpg'
+import dragDropThumbnail from '../assets/images/dragndrop_illustration.gif'
 import pic8 from '../assets/images/pic08.jpg'
 import Scroll from '../components/Scroll'
 import { graphql, useStaticQuery } from 'gatsby'
@@ -79,58 +74,12 @@ const IndexPage = ({ location, navigate }) => {
             </header>
 
             <div className="row">
-              <div className="col-4 col-12-mobile">
-                <article className="item">
+              <div className="col-12 col-12-mobile">
+                <article className="portfolio__item item">
+                  <h3 className="portfolio__header">Drag and Drop - ReactJS</h3>
                   <a href={dragDropPath} className="image fit">
-                    <img src={pic2} alt="Drag and Drop Thumbnail" />
+                    <img src={dragDropThumbnail} alt="Drag and Drop Thumbnail" />
                   </a>
-                  <header>
-                    <h3>Drag and Drop</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="/#" className="image fit">
-                    <img src={pic3} alt="" />
-                  </a>
-                  <header>
-                    <h3>Rhoncus Semper</h3>
-                  </header>
-                </article>
-              </div>
-              <div className="col-4 col-12-mobile">
-                <article className="item">
-                  <a href="/#" className="image fit">
-                    <img src={pic4} alt="" />
-                  </a>
-                  <header>
-                    <h3>Magna Nullam</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="/#" className="image fit">
-                    <img src={pic5} alt="" />
-                  </a>
-                  <header>
-                    <h3>Natoque Vitae</h3>
-                  </header>
-                </article>
-              </div>
-              <div className="col-4 col-12-mobile">
-                <article className="item">
-                  <a href="/#" className="image fit">
-                    <img src={pic6} alt="" />
-                  </a>
-                  <header>
-                    <h3>Dolor Penatibus</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="/#" className="image fit">
-                    <img src={pic7} alt="" />
-                  </a>
-                  <header>
-                    <h3>Orci Convallis</h3>
-                  </header>
                 </article>
               </div>
             </div>
