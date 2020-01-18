0496caa5e74d33962b7b64c468e68bdd2700d90d
Benard Kong <benardkong@hotmail.com>
2020-01-18 13:24:22 -0800

    [dnd-3-link-with-splash-page] Fix rel=noopener security warning

--- src/components/SideBar/Footer.js
+++ src/components/SideBar/Footer.js
@@ -6,9 +6,15 @@ export default function Footer({ socialLinks = [] }) {
       <ul className="icons">
         {socialLinks.map((social) => {
           const { icon, name, url, target } = social
+          const newTabProps = {}
+          if (target === '_blank') {
+            newTabProps.target = target
+            newTabProps.rel = 'noopener noreferrer'
+          }
+
           return (
             <li key={url}>
-              <a href={url} className={`icon ${icon}`} target={target}>
+              <a href={url} className={`icon ${icon}`} {...newTabProps}>
                 <span className="label">{name}</span>
               </a>
             </li>
