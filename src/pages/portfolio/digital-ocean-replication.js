f7ec94655ddea350cb481872539db140f7bd91b5
Benard Kong <benardkong@hotmail.com>
2020-01-25 22:24:51 -0800

    [digital-ocean-1-init] Add links to original Digital Ocean and README

--- src/pages/portfolio/digital-ocean-replication.js
+++ src/pages/portfolio/digital-ocean-replication.js
@@ -9,8 +9,14 @@ const sections = [
     name: 'Back to Portfolio',
   },
   {
+    id: 'compare-to-original',
+    url: 'https://web.archive.org/web/20190528080206/https://www.digitalocean.com/',
+    icon: 'fa-home',
+    name: 'Compare with digitalocean.com',
+  },
+  {
     id: 'see-source-code',
-    url: 'https://github.com/bkong88/',
+    url: 'https://github.com/bkong88/portfolio-kong-benard/tree/master/src/portfolio/DigitalOceanReplication',
     icon: 'fa-file-code-o',
     name: 'Source Code',
   },
