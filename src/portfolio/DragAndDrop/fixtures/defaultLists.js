c69903335c7348f9b5b0467080c58b80751ed229
Benard Kong <benardkong@hotmail.com>
2020-01-26 09:55:01 -0800

    [dnd-6-text-entry-font] Remove tech stack list

--- src/portfolio/DragAndDrop/fixtures/defaultLists.js
+++ src/portfolio/DragAndDrop/fixtures/defaultLists.js
@@ -1,5 +1,5 @@
 import uuid_v1 from 'uuid/v1'
-import { createDefaultCards, createInstructionCards, createTechStackCards } from './defaultCards'
+import { createDefaultCards, createInstructionCards } from './defaultCards'
 
 const createList = (title = '', cardIdsArray = []) => {
   const listId = uuid_v1()
@@ -14,9 +14,9 @@ export const createDefaultLists = () => {
 
   // Create lists for instructions
   const instructionsList = createList('Instructions')
-  const techStackList = createList('Packages Used (for developers)')
+  // const techStackList = createList('Packages Used (for developers)')
   listsObject[instructionsList.listId] = instructionsList.listObject
-  listsObject[techStackList.listId] = techStackList.listObject
+  // listsObject[techStackList.listId] = techStackList.listObject
 
   // Create lists
   const list1 = createList('Backlog')
@@ -26,13 +26,14 @@ export const createDefaultLists = () => {
   listsObject[list2.listId] = list2.listObject
   listsObject[list3.listId] = list3.listObject
 
-  const listsArray = [instructionsList.listId, techStackList.listId, list1.listId, list2.listId, list3.listId]
+  const listsArray = [instructionsList.listId, list1.listId, list2.listId, list3.listId]
+  // const listsArray = [instructionsList.listId, techStackList.listId, list1.listId, list2.listId, list3.listId]
 
   // Add cards for instructions
   const instructionsListCards = createInstructionCards(instructionsList.listId)
-  const techStackListCards = createTechStackCards(techStackList.listId)
+  // const techStackListCards = createTechStackCards(techStackList.listId)
   listsObject[instructionsList.listId].cardIdsArray = instructionsListCards.cardIdsArray
-  listsObject[techStackList.listId].cardIdsArray = techStackListCards.cardIdsArray
+  // listsObject[techStackList.listId].cardIdsArray = techStackListCards.cardIdsArray
 
   // Add cards
   const list1Cards = createDefaultCards(list1.listId, list1.listObject.title, 4)
@@ -45,7 +46,7 @@ export const createDefaultLists = () => {
   // Combine all cards objects
   const cardsObject = {
     ...instructionsListCards.cardsObject,
-    ...techStackListCards.cardsObject,
+    // ...techStackListCards.cardsObject,
     ...list1Cards.cardsObject,
     ...list2Cards.cardsObject,
     ...list3Cards.cardsObject,
