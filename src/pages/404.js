291d87f905f813f8494931f533a107e052329315
Benard Kong <benardkong@hotmail.com>
2020-01-31 07:28:30 -0800

    [not-found-url-1-init] Not found page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import { withSidebar } from '../hoc/withSidebar'
 
-import Layout from '../components/Layout'
-import SideBar from '../components/SideBar'
-
-const IndexPage = () => (
-  <Layout>
-    <SideBar />
-    <div id="wrapper">
-      <div id="main">
-        <section>
-          <div className="container">
-            <section>
-              <h1>NOT FOUND</h1>
-              <p>Not a valid URL</p>
-            </section>
-          </div>
-        </section>
-      </div>
-    </div>
-  </Layout>
+const NotFoundPage = () => (
+  <div className="url-not-found">
+    <h1 className="url-not-found__title">NOT FOUND</h1>
+    <p className="url-not-found__paragraph">The URL you have entered is not a valid URL.</p>
+    <p className="url-not-found__paragraph">
+      <a href="/" className="url-not-found__to-home">
+        Click here
+      </a>{' '}
+      to go to the home page.
+    </p>
+  </div>
 )
 
-export default IndexPage
+const sections = [
+  {
+    id: 'to-splash-page',
+    url: '/',
+    icon: 'fa-home',
+    name: 'Go to Home Page',
+  },
+]
+const scrollSpy = false
+const withSidebarProps = { sections, scrollSpy }
+const mainDivClassNames = 'digital-ocean-main-container'
+
+export default withSidebar(NotFoundPage, withSidebarProps, mainDivClassNames)
